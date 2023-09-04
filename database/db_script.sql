4148af61227a5b2cc4f1994e467b40eac5ef58e4
Zelkiron <140980003+Zelkiron@users.noreply.github.com>
2023-09-04 03:09:51 -0400

    updated script for creating database also testing this on laptop

--- database/db_script.sql
+++ database/db_script.sql
@@ -6,30 +6,30 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `rank` tinyint(1) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `act_code` varchar(255) NOT NULL,
-  `temp_code` varchar(255) DEFAULT NULL,
-  `num_posts` int NOT NULL,
-  `status` text NOT NULL,
+  `activation_code` varchar(255) NOT NULL,
+  `code_to_change_password` varchar(255) NULL DEFAULT NULL,
+  `number_of_posts` int NOT NULL,
+  `status` text NULL DEFAULT NULL,
   `topics` int NOT NULL,
-  `rep` int NOT NULL,
-  `pfp` text NOT NULL,
+  `reputation` int NOT NULL,
+  `profile_picture` text NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cat_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `topic_id` int(11) NOT NULL,
   `poster_id` int(11) NOT NULL,
   `post_order` int(11) NOT NULL DEFAULT 1,
   `name` varchar(50) NOT NULL,
   `content` text NOT NULL,
-  `rep` int(11) NOT NULL DEFAULT 0,
+  `reputation` int(11) NOT NULL DEFAULT 0,
   `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
   `date_edited` timestamp NULL DEFAULT NULL,
   `date_new_reply` timestamp NOT NULL DEFAULT current_timestamp(),
   `replies` int(11) NOT NULL DEFAULT 0,
   `visibility` tinyint(1) NOT NULL,
-  `isLocked` tinyint(1) NOT NULL DEFAULT 0,
+  `is_locked` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `status_updates` (
@@ -39,15 +39,15 @@ CREATE TABLE `status_updates` (
   `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
   `date_edited` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `visibility` tinyint(1) NOT NULL,
-  `isLocked` tinyint(1) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `desc` text NOT NULL,
+  `description` text NOT NULL,
   `topics` int(11) NOT NULL,
-  `replies` int(11) NOT NULL,
+  `posts` int(11) NOT NULL,
   `visibility` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
